d4d79d917b336f29ad1fdca847e308ff6851dac2
Oles Maiboroda <oles.maiboroda@gmail.com>
2024-10-29 12:33:15 +0100

    Remove get started folder

--- restack-get-started/rebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Remove the restack_ai package from the virtual environment
-poetry remove restack_ai
-
-# Readd the restack_ai package from the local path
-poetry add restack_ai
