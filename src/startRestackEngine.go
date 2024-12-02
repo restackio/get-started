e4f4290929a36a95b14a9ab9408e9474d90da98c
martinibach <martin@restack.io>
2024-12-02 13:10:49 +0100

    Refactor Docker container name and image in startRestackEngine.go to restack/restack

--- src/startRestackEngine.go
+++ src/startRestackEngine.go
@@ -7,12 +7,12 @@ import (
 )
 
 func (m model) startRestackEngine() error {
-	cmd := exec.Command("docker", "rm", "-f", "studio")
+	cmd := exec.Command("docker", "rm", "-f", "restack")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
 
-	cmd = exec.Command("docker", "run", "-d", "--pull", "always", "--name", "studio", "-p", "5233:5233", "-p", "6233:6233", "-p", "7233:7233", "ghcr.io/restackio/engine:main")
+	cmd = exec.Command("docker", "run", "-d", "--pull", "always", "--name", "restack", "-p", "5233:5233", "-p", "6233:6233", "-p", "7233:7233", "ghcr.io/restackio/restack:main")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
